75085fe94fa4ab69171c28af4f0722dafd50cd4c
i-ece <34932574+i-ece@users.noreply.github.com>
2017-12-29 17:47:57 +0900

    Update sensor.js

--- accessories/sensor.js
+++ accessories/sensor.js
@@ -186,7 +186,7 @@ function HomeAssistantSensorFactory(log, data, client) {
     service = Service.CarbonDioxideSensor;
     characteristic = Characteristic.CarbonDioxideLevel;
     //AQI + PM2_5 추가
-  } else if ((typeof data.attributes.unit_of_measurement === 'string' && data.attributes.unit_of_measurement.toLowerCase() === '㎍/㎥') || data.attributes.homebridge_sensor_type === 'pm10density') {
+  } else if ((typeof data.attributes.unit_of_measurement === 'string' && data.attributes.unit_of_measurement.toLowerCase() === '㎍/㎥') || data.attributes.homebridge_sensor_type === 'PM2_5Density') {
     service = Service.AirQualitySensor;
     characteristic2 = Characteristic.PM2_5Density;
     transformData2 = function transformData(dataToTransform) {
