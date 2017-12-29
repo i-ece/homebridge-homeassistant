56677c4742dd8970c1b172fd4259794671e2d592
i-ece <34932574+i-ece@users.noreply.github.com>
2017-12-29 18:02:09 +0900

    Update sensor.js

--- accessories/sensor.js
+++ accessories/sensor.js
@@ -192,12 +192,14 @@ function HomeAssistantSensorFactory(log, data, client) {
     //AQI + PM2_5 추가
   } else if ((typeof data.attributes.unit_of_measurement === 'string' && data.attributes.unit_of_measurement.toLowerCase() === '㎍/㎥') || data.attributes.homebridge_sensor_type === 'PM2_5Density') {
     service = Service.AirQualitySensor;
-
     characteristic = Characteristic.AirQuality;
-    characteristic2 = Characteristic.PM2_5Density;    
+    characteristic2 = Characteristic.PM2_5Density;
+    transformData2 = function transformData2(dataToTransform) {
+      const value = parseFloat(dataToTransform.state);
+      return value;
+    };        
     transformData = function transformData(dataToTransform) { // eslint-disable-line no-shadow
       const value = parseFloat(dataToTransform.state);
-      transformData2 = value;
       if (value <= 30) {
         return 1;
       } else if (value >= 31 && value <= 70) {
